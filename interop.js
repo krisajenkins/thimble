9713f8ed193cbb246d0f785d494a3ecfe8be7af8
Kris Jenkins <krisajenkins@gmail.com>
2015-04-04 22:30:16 +0100

    Initial commit.

---
+++ interop.js
@@ -0,0 +1,44 @@
+/* @flow weak */
+
+(function () {
+    var STARRED_PRODUCTS_KEY = "STARRED_PRODUCTS";
+
+    var getScroll = function () {
+        return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+    };
+
+    var sendHash = function (event) {
+        app.ports.locationHash.send(document.location.hash);
+    };
+
+    var sendScroll = function(event) {
+        app.ports.scrollPercentage.send(getScroll());
+    };
+
+    var localStorageEvent = function(event) {
+        if (event.key === STARRED_PRODUCTS_KEY) {
+            app.ports.starredProductsRead.send(event.newValue);
+        }
+    };
+
+    var starredProductsSave = function (string) {
+        if (string) {
+            window.localStorage.setItem(STARRED_PRODUCTS_KEY, string);
+        }
+    };
+
+    window.addEventListener('load', sendHash, false);
+    window.addEventListener('popstate', sendHash, false);
+    window.addEventListener('scroll', sendScroll, false);
+    window.addEventListener('storage', localStorageEvent, false);
+
+    var app = Elm.fullscreen(
+        Elm.Main,
+        {locationHash : document.location.hash,
+         scrollPercentage: getScroll(),
+         starredProductsRead: ""
+        });
+
+    app.ports.starredProductsWrite.subscribe(starredProductsSave);
+    app.ports.starredProductsRead.send(window.localStorage.getItem(STARRED_PRODUCTS_KEY));
+}());
